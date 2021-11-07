cc01421d20d519badb4dac398193a6ca7b2a291c
Sagar Sondur <32958109+sagarsakri@users.noreply.github.com>
2021-11-07 09:52:13 +0530

    fetching data in frontend - MatchPage

--- src/frontend/src/pages/MatchPage.jsx
+++ src/frontend/src/pages/MatchPage.jsx
@@ -1,10 +1,35 @@
-import React from 'react'
+import React, { useEffect, useState } from 'react'
+import { MatchDetailCard } from '../Components/MatchDetailCard'
+import { MatchSmallCard } from '../Components/MatchSmallCard'
+import { useParams } from 'react-router';
 
 export const MatchPage = () => {
+    const [matches, setMatches] = useState([]);
+    const { teamName,year } = useParams();
+    // const teamName = "Delhi Capitals";
+
+    useEffect(() => {
+        const fetchMatches = async () => {
+            const response = await fetch(`http://localhost:8080/team/${teamName}/matches?year=${year}`);
+            const data = await response.json();
+            console.log(data);
+            setMatches(data)
+        };
+        fetchMatches();
+
+    }, [teamName]);
+
     return (
 
         <div className="MatchPage">
             <h2>Matches</h2>
+            {
+                matches.map(match => {
+                    return (
+                        <MatchDetailCard key={match.id} teamName={teamName} match={match} />
+                    )
+                })
+            }
         </div>
     )
 }
