f8ce62a1d64125e10ccf8514f7937b3f2b235bac
Sagar Sondur <32958109+sagarsakri@users.noreply.github.com>
2021-11-09 10:35:28 +0530

    styling the TeamPage Component

--- src/frontend/src/pages/TeamPage.jsx
+++ src/frontend/src/pages/TeamPage.jsx
@@ -2,6 +2,8 @@ import React, { useEffect, useState } from 'react'
 import { MatchDetailCard } from '../Components/MatchDetailCard'
 import { MatchSmallCard } from '../Components/MatchSmallCard'
 import { useParams } from 'react-router';
+import { PieChart } from 'react-minimal-pie-chart';
+import './TeamPage.scss';
 
 export const TeamPage = () => {
     const [team, setTeam] = useState({ matches: [] });
@@ -22,12 +24,26 @@ export const TeamPage = () => {
     if (!team || !team.teamName) {
         return <h2>Team Not Found</h2>
     }
-
+    console.log(team);
     return (
 
         <div className="TeamPage">
-            <h3>{team.teamName}</h3>
-            <MatchDetailCard teamName={team.teamName} match={team.matches[0]} />
+            <div className="team-name-section" >
+                <h1 className="team-name">{team.teamName}</h1>
+            </div>
+            <div className="win-loss-section" >
+                Win / losses
+                <PieChart
+                    data={[
+                        { title: 'Losses', value: team.totalMatchesPlayed - team.totalWins, color: 'rgb(92, 20, 20)' },
+                        { title: 'Wins', value:  team.totalWins, color: 'rgb(68, 146, 32)' },
+                    ]}
+                />
+            </div>
+            <div className="match-detail-section" >
+                <h3>Latest Matches</h3>
+                <MatchDetailCard teamName={team.teamName} match={team.matches[0]} />
+            </div>
             {
                 team.matches.slice(1).map(match => {
                     return (
@@ -35,6 +51,9 @@ export const TeamPage = () => {
                     )
                 })
             }
+            <div className="more-link">
+                <a href="#">More > </a>
+            </div>
         </div>
     )
 }
